d788f82aabf0ba1756e0c096b8ab71d2c16c34ea
raythurman2386 <raymondthurman5@gmail.com>
2019-12-08 18:34:40 -0500

    basic comment router set up, needs logic

---
+++ routers/commentRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+// GET Comments
+router.get("/:id/comments", (req, res) => {});
+
+// POST Comment
+router.post("/:id/comments", (req, res) => {});
+
+module.exports = router;
