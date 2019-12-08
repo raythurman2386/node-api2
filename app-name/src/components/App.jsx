028a2c191a8cdfa7d743d24ad99aef2cef3d6ee9
raythurman2386 <raymondthurman5@gmail.com>
2019-12-08 18:42:13 -0500

    react-wizard cra added

---
+++ app-name/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {CssBaseline, Typography} from '@material-ui/core';
+
+function App() {
+    return (
+        <>
+            <CssBaseline/>
+            <Typography variant="h1">Welcome to the React Wizard Template</Typography>
+        </>
+    )
+}
+
+export default App;
---
+++ app-name/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import App from './components/App';
+import './index.scss';
+
+render(<Router><App/></Router>, document.getElementById('root'));
