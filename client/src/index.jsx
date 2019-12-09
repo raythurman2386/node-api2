7d4a263fd49699ab64fff935b1e512c20405882f
raythurman2386 <raymondthurman5@gmail.com>
2019-12-08 20:41:39 -0500

    reformated client

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import App from './components/App';
-import './index.scss';
+import App from "./components/App";
+import "./index.scss";
 
-render(<Router><App/></Router>, document.getElementById('root'));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
