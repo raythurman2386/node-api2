715492a930c6e885d670c0877dd37eb8e7222af9
raythurman2386 <raymondthurman5@gmail.com>
2019-12-08 19:37:22 -0500

    get posts comments working

--- routers/commentRouter.js
+++ routers/commentRouter.js
@@ -6,12 +6,28 @@ const router = express.Router({
 });
 
 // Test route
-router.get("/", (req, res) => {
-  res.send("<h2>Hello from the comment route</h2>");
-});
+// router.get("/", (req, res) => {
+//   res.send("<h2>Hello from the comment route</h2>");
+// });
 
 // GET Comments
-router.get("/:id/comments", (req, res) => {});
+router.get("/", (req, res) => {
+  db.findPostComments(req.params.id)
+    .then(comments => {
+      if (comments.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "The post with the specified ID Does not exist" });
+      }
+
+      res.status(200).json(comments);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ errorMessage: "The comments info could not be retrieved" });
+    });
+});
 
 // POST Comment
 router.post("/:id/comments", (req, res) => {});
