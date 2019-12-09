7ceffc871ca4c83678e9a29aeaa0d691e48496df
raythurman2386 <raymondthurman5@gmail.com>
2019-12-08 19:28:52 -0500

    put route working as it should

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -79,6 +79,28 @@ router.delete("/:id", (req, res) => {
 });
 
 // PUT Posts
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide title and contents for the post" });
+  }
+
+  db.update(req.params.id, req.body)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be modified." });
+    });
+});
 
 module.exports = router;
