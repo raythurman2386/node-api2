7871f79b83a11c7265e8718ca107a1360ea360d0
Ray Thurman <raymondthurman5@gmail.com>
2019-12-10 10:49:19 -0500

    updated test router

--- api/routers/testRouter.js
+++ api/routers/testRouter.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("<h2>Welcome to your express router</h2>");
+router.get("/", async (req, res) => {
+  try {
+    const shoutouts = await db("shoutouts");
+    const messageOfTheDay = process.env.MOTD || "Hello World!"; // add this line
+    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+  } catch (error) {
+    console.error("\nERROR", error);
+    res.status(500).json({ error: "Cannot retrieve the shoutouts" });
+  }
 });
 
 router.get("/api", (req, res) => {
